3fbef818707d40693e2628521bddc90d1ab083f0
SamiraPorkhaligh <samira.pourkhaligh@gmail.com>
2022-08-21 05:26:36 +0430

    add navigation

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const image = document.querySelector("#image-card");
+const category = document.querySelector(".category");
+const typeCategory = document.querySelector(".type-category");
+const titleCard = document.querySelector(".title-card");
+const priceCard = document.querySelector(".price-card");
+const summaryCard = document.querySelector(".summary-card");
+const save = document.querySelector(".save");
+
+
+ 
+
+
